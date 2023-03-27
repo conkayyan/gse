e7223142f4b3d895ce60cd6e36071ce0b769f8da
conkayyan <conkay@foxmail.com>
2023-03-27 22:45:20 +0800

    Update dict_embed.go

--- dict_embed.go
+++ dict_embed.go
@@ -3,22 +3,12 @@
 
 package gse
 
-import (
-	_ "embed"
-)
-
 var (
-	//go:embed data/dict/jp/dict.txt
 	ja string
 
-	//go:embed data/dict/zh/t_1.txt
 	zhT string
-	//go:embed data/dict/zh/s_1.txt
 	zhS string
-
-	//go:embed data/dict/zh/idf.txt
 	ZhIdf string
 )
 
-//go:embed data/dict/zh/stop_tokens.txt
 var stopDict string
